aaf66b3eb324b1b63d5e24c1e103ed86242a3ea6
Eugeny Okulik <tiburon854@gmail.com>
2023-11-24 20:43:35 +0300

    "'--disable-dev-shm-usage'" added

--- conftest.py
+++ conftest.py
@@ -11,6 +11,7 @@ def driver():
     options = Options()
     options.add_argument("--headless")
     options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     chrome_driver = webdriver.Chrome(options=options)
     sleep(3)
     return chrome_driver
